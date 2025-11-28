8150bb5edb46aaec2fbae35f1b6e5b389f7857e3
rutaku12rutaku12 <chookyuho@gmail.com>
2025-11-29 02:07:03 +0900

    게시판 번호, 제목, 가통신

--- app/(tabs)/writing.tsx
+++ app/(tabs)/writing.tsx
@@ -1,19 +1,36 @@
+import { useEffect, useState } from "react";
 import { Dimensions ,StyleSheet, View , Text, FlatList} from "react-native";
 
 export default function HomeScreen() {
 
-  const posts : {id: number; title: string}[] = [
-    {id:1, title:"게시글1"},
-    {id:2, title:"게시글2"},
-    {id:3, title:"게시글3"},
-    {id:4, title:"게시글4"},
-    {id:5, title:"게시글5"},
-    {id:6, title:"게시글6"},
-    {id:7, title:"게시글7"},
-    {id:8, title:"게시글8"},
-    {id:9, title:"게시글9"},
-    {id:10, title:"게시글10"},
-  ];
+  const [posts, setPosts] = useState<{id: number; title: string}[]> ([]);
+
+//     useEffect(() => {
+//       setPosts([
+//         {id:1, title:"게시글1"},
+//         {id:2, title:"게시글2"},
+//         {id:3, title:"게시글3"},
+//         {id:4, title:"게시글4"},
+//         {id:5, title:"게시글5"},
+//         {id:6, title:"게시글6"},
+//         {id:7, title:"게시글7"},
+//         {id:8, title:"게시글8"},
+//         {id:9, title:"게시글9"},
+//         {id:10, title:"게시글10"},
+//   ]);
+// },[]);
+
+useEffect(()=> {
+  const fetchPosts = async () => {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    const data = await response.json();
+    console.log(data);
+    setPosts(data);
+  };
+  fetchPosts();
+}, []);
 
   return (<>
     <View style={styles.postsContainer}>
@@ -22,7 +39,10 @@ export default function HomeScreen() {
       keyExtractor={post => post.id.toString()}
       contentContainerStyle={styles.listWrap}
       renderItem={({item}) => (
-        <Text style={styles.postItem}>{item.title}</Text>
+        <View style={styles.postItem}>
+          <Text style={styles.postId}>{item.id}번 게시글</Text>
+          <Text style={styles.postTitle}>{item.title}</Text>
+        </View>
       )}
      />
     </View>
@@ -42,6 +62,13 @@ const styles = StyleSheet.create({
     paddingBottom: 16,
     paddingHorizontal: 6,
   },
+  postId:{
+    fontSize:16,
+    fontWeight:"bold",
+  },
+  postTitle:{
+    fontSize:16,
+    marginTop:5,},
   postItem:{
     backgroundColor: "#fff",
     padding:16,
