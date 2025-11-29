77690fc31b58d066c4ac1339e165b0f16382a484
rutaku12rutaku12 <chookyuho@gmail.com>
2025-11-29 19:17:26 +0900

    env 깃 이그노어

--- types/firebase/config.ts
+++ types/firebase/config.ts
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "---",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
   authDomain: "catchuapp.firebaseapp.com",
   projectId: "catchuapp",
   storageBucket: "catchuapp.firebasestorage.app",
